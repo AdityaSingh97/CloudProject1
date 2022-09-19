abaf167fdca88d6516ee059596fac4f61cca951e
isha-np <74524978+isha-np@users.noreply.github.com>
2022-09-19 15:14:28 -0700

    added autoscaling function

--- controller.py
+++ controller.py
@@ -1,29 +1,37 @@
 #libraries to import
+from audioop import maxpp
 import socket
 import logging
 import boto3
 from botocore.exceptions import ClientError
+import json
+import time
 
 #create client for ec2
 ec2_client=boto3.client('ec2',region_name='us-east-1')
 ec2_resrc=boto3.resource('ec2',region_name='us-east-1')
 
 #define constants
+#to add key pair and security group ID
 APP_TIER_PREFIX='app_tier_ec2'
+MAX_APP_EC2=19
+KEY_PAIR=0
+SEC_GROUP_ID=0
 
 #launch ec2 instances
-def create_instance(key_pair,security_group_id,img_id='ami-0bb1040fdb5a076bc'):
+#check --> min=1 required? 
+def create_instance(key_pair,security_group_id,img_id='ami-0bb1040fdb5a076bc',min,max):
     instance_list=ec2_resrc.create_instances(
         ImageID=img_id,
-        MinCount=1,
-        MaxCount=1,
+        MinCount=min,
+        MaxCount=max,
         InstanceType='t2-micro',
         KeyName=key_pair,
         SecurityGroupIds=[security_group_id]
         #need to insert bash script which automates the start of web tier
     )
     #create instance tags for each instance by storing values as {'app-tier-ec2',instance number} until we arrive at 19 instances
-    for i in range(1):
+    for i in range(max):
         j=i+1
         inst_id=instance_list[i]
         print(inst_id)
@@ -43,5 +51,27 @@ def create_instance(key_pair,security_group_id,img_id='ami-0bb1040fdb5a076bc'):
         id_list.append(inst_id)
         ec2_resrc.instances.filter(InstanceIds=id_list).terminate()
         
+    #autoscaling function
+    def autoscale(url):
+        #get length of queue with url given
+        queue_approx_num_msgs=get_queue_attributes(QueueUrl=url,AttributeNames='ApproximateNumberOfMessages')['Attributes']
+        #convert to integer
+        queue_len=int(queue_approx_num_msgs)
+        #get number of running ec2 instances
+        num_running_instances=get_running_instances()
+        
+        #launch 19-number of running instances
+        num_ec2_launch=MAX_APP_EC2-num_running_instances
+        #create as many instances as calculated
+        create_instance(KEY_PAIR,SEC_GROUP_ID,'ami-0bb1040fdb5a076bc',min=num_ec2_launch,max=num_ec2_launch)
+    
+    #function to get the number of running instances
+    def get_running_instances():
+        instances_run=ec2_resrc.instances.filter(
+            Filters=[{'Name':'instance-state-name','Values':['running']}])
+        instance_ids=[]
+        for instance in instance_ids:
+            instance_ids.append(instance)
+        return len(instance_ids)
         
     
