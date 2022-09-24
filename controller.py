b7b111fbb42905c2fa3eb8e2e56b25e9be9beb48
isha-np <74524978+isha-np@users.noreply.github.com>
2022-09-24 16:39:19 -0700

    corrected errors

--- controller.py
+++ controller.py
@@ -50,36 +50,36 @@ def create_instance(key_pair,security_group_id,img_id,min,max):
     #terminate instance 
     #add instance id of instance to be terminated into an empty array 
     #filter function from boto3 requires passing an array of instance IDs
-    def terminate_instance(inst_id):
-        id_list=[]
-        id_list.append(inst_id)
-        ec2_resrc.instances.filter(InstanceIds=id_list).terminate()
+def terminate_instance(inst_id):
+    id_list=[]
+    id_list.append(inst_id)
+    ec2_resrc.instances.filter(InstanceIds=id_list).terminate()
         
     #autoscaling function
     #to check get_queue_attributes function parameters 
     #TODO check if get_one_queue_attribute() function can be used from app-helper.py
-    def autoscale(url):
-        #get length of queue with url given, which denotes the number of req on the SQS
-        queue_approx_num_msgs=sqs_client.get_queue_attributes(QueueUrl=url,AttributeNames='ApproximateNumberOfMessages')['Attributes']
-        #convert to integer
-        queue_len=int(queue_approx_num_msgs)
-        #get number of running ec2 instances
-        num_running_instances=get_running_instances()
+def autoscale(url):
+    #get length of queue with url given, which denotes the number of req on the SQS
+    queue_approx_num_msgs=sqs_client.get_queue_attributes(QueueUrl=url,AttributeNames='ApproximateNumberOfMessages')['Attributes']
+    #convert to integer
+    queue_len=int(queue_approx_num_msgs)
+    #get number of running ec2 instances
+    num_running_instances=get_running_instances()
         
-        #launch 19-number of running instances
-        max_ec2_launch=MAX_APP_EC2-num_running_instances
-        #take the minimum of number of requests and value calculated above
-        num_ec2_launch=min(queue_len,max_ec2_launch)
-        #create as many instances as calculated
-        create_instance(KEY_PAIR,SEC_GROUP_ID,'ami-0bb1040fdb5a076bc',min=num_ec2_launch,max=num_ec2_launch)
+    #launch 19-number of running instances
+    max_ec2_launch=MAX_APP_EC2-num_running_instances
+    #take the minimum of number of requests and value calculated above
+    num_ec2_launch=min(queue_len,max_ec2_launch)
+    #create as many instances as calculated
+    create_instance(KEY_PAIR,SEC_GROUP_ID,'ami-0bb1040fdb5a076bc',min=num_ec2_launch,max=num_ec2_launch)
     
-    #function to get the number of running instances
-    def get_running_instances():
-        instances_run=ec2_resrc.instances.filter(
-            Filters=[{'Name':'instance-state-name','Values':['running']}])
-        instance_ids=[]
-        for instance in instance_ids:
-            instance_ids.append(instance)
-        return len(instance_ids)
+#function to get the number of running instances
+def get_running_instances():
+    instances_run=ec2_resrc.instances.filter(
+        Filters=[{'Name':'instance-state-name','Values':['running']}])
+    instance_ids=[]
+    for instance in instance_ids:
+        instance_ids.append(instance)
+    return len(instance_ids)
         
     
